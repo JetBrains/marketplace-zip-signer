3a2bce8ae1933ebc0863060e92b7f251ce774401
satamas <semyon.atamas@jetbrains.com>
2020-02-13 11:44:21 +0300

    Use groovy gradle scripts. Set target java version for all projects

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    kotlin("jvm") version "1.3.61" apply false
-}
-
-subprojects {
-    repositories {
-        mavenCentral()
-    }
-}
--- lib/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    kotlin("jvm")
-}
-
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    api("org.bouncycastle", "bcpkix-jdk15on", "1.64")
-}
-
