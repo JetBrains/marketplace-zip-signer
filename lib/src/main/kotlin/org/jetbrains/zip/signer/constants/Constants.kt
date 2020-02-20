91106dbeea84555e772646279b09ebb7576a579e
satamas <semyon.atamas@jetbrains.com>
2020-02-20 10:28:54 +0300

    Minor: cleanup

--- lib/src/main/kotlin/org/jetbrains/zip/signer/constants/Constants.kt
+++
@@ -1,3 +0,0 @@
-package org.jetbrains.zip.signer.constants
-
-public const val SIGNATURE_SCHEME_BLOCK_ID = 0x7109871a;
--- lib/src/main/kotlin/org/jetbrains/zip/signer/exceptions/PluginFormatException.kt
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.zip.signer.exceptions
-
-class PluginFormatException : Exception {
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-}
