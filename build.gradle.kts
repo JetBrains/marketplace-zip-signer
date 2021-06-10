b49efd146449bb2658da53b536fb82ae7ac8a214
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2021-06-10 15:13:59 +0200

    Revert "use Gradle Nexus Publish Plugin for automated publishing"
    This reverts commit ef1ac885ac28d9fab342eed1891803abb1869554.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,5 @@
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            val mavenCentralUsername: String? by project
-            val mavenCentralPassword: String? by project
-
-            username.set(mavenCentralUsername)
-            password.set(mavenCentralPassword)
-        }
-    }
 }
 
 allprojects {
