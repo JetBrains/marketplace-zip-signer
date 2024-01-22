791175f36e272cbc262c5962b560920d323a74c5
Sergey.Kesarev <sergey.kesarev@jetbrains.com>
2024-01-22 16:04:11 +0100

    Bump kotlin to 1.7.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.32"
+    kotlin("jvm") version "1.7.22"
     id("maven-publish")
     id("signing")
     id("com.github.johnrengelman.shadow") version "7.1.2"
