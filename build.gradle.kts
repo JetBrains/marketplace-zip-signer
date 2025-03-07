8240c8e34f76725760bdf101b7f1d1923787c403
Sergey.Kesarev <sergey.kesarev@jetbrains.com>
2025-03-07 19:29:27 +0100

    bump kotlin jvm plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.9.25"
     id("maven-publish")
     id("signing")
     id("com.gradleup.shadow") version "8.3.6"
